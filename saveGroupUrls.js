2f40f39662068576efa68f90e6c6203d1fa8e11d
shyr1punk <shyr1punk@gmail.com>
2016-07-06 01:00:02 +0300

    Добавлена запись в mongodb списка групп

---
+++ saveGroupUrls.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+const async = require('async');
+
+const getScheduleFilesUrls = require('./url-parser').getScheduleFilesUrls;
+const specialityUrls = require('./url-parser/specialityUrls');
+
+// Connection URL
+const url = 'mongodb://localhost:27017/schedule';
+// Use connect method to connect to the Server
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected correctly to server");
+  const collection = db.collection('groups');
+  async.forEach(specialityUrls, (specialityUrl, callback) => {
+    getScheduleFilesUrls(specialityUrl, groups => {
+      const groupsCount = groups.length;
+      if(!groupsCount) {
+        callback(null, db);
+        return;
+      }
+      collection.insertMany(groups, function(err, result) {
+        assert.equal(err, null);
+        assert.equal(groupsCount, result.result.n);
+        assert.equal(groupsCount, result.ops.length);
+        console.log(`Inserted ${groupsCount} groups into the groups collection`);
+        callback(null, db);
+      });
+    });
+  }, err => {
+    db.close();
+  });
+});
+
--- url-parser/index.js
+++ url-parser/index.js
@@ -3,17 +3,16 @@
 const request = require('request');
 const iconv = require('iconv-lite');
 
-const specialityUrls = require('./specialityUrls');
 const getGroupURLMapping = require('./getGroupUrls').getGroupURLMapping;
 const getScheduleFilesArray = require('./getGroupUrls').getScheduleFilesArray;
 
-/**
- * Запускать для каждого URL xls файла группы
- */
-
-specialityUrls.map(url => {
-  request(encodeURI(url)).pipe(iconv.decodeStream('win1251')).collect(function(err, body) {
+function getScheduleFilesUrls(specialityUrls, callback) {
+  request(encodeURI(specialityUrls)).pipe(iconv.decodeStream('win1251')).collect(function(err, body) {
     const groups = getScheduleFilesArray(body);
-    console.log(getGroupURLMapping(groups));
+    callback(getGroupURLMapping(groups));
   });
-})
+}
+
+module.exports = {
+  getScheduleFilesUrls
+}
