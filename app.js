be2bcea3fa4257c09b583dfd1425efd83a0c0329
shyr1punk <shyr1punk@gmail.com>
2016-08-23 22:32:54 +0300

    Добавдено простое express приложение, отображающее  список групп

---
+++ app.js
@@ -0,0 +1,30 @@
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+const mongodbConnectUrl = require('./config.js').mongodbConnectUrl;
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.json({
+    test: 1
+  });
+});
+
+app.get('/groups', (req, res) => {
+  MongoClient.connect(mongodbConnectUrl, function(err, db) {
+    assert.equal(null, err);
+    console.log("Connected correctly to server");
+    const groups = db.collection('groups');
+    groups.find({}).toArray((err, groups) => {
+      assert.equal(null, err);
+      assert.ok(groups != null);
+      res.json(groups);
+    });
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Schedule app listen at 3000');
+});
